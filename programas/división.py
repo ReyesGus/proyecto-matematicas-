e5a2f3dcfdeae4271e2cf037160c4e3a59194f81
ReyesGus <50606224+ReyesGus@users.noreply.github.com>
2021-10-24 21:13:54 -0500

    Programas

---
+++ programas/división.py
@@ -0,0 +1,7 @@
+n1 = int(input("Introduce el primer número: "))
+n2 = int(input("Introduce el segudno numero: "))
+
+division = n1 / n2
+x = print("el resultado es: ")
+y = print(input(division))
+
---
+++ programas/resta.py
@@ -0,0 +1,6 @@
+n1 = int(input("Introduce el primer número: "))
+n2 = int(input("Introduce el segudno numero: "))
+
+resta = n1 - n2
+x = print("el resultado es: ")
+y = print(input(resta))
